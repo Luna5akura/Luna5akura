8e29b09ba58e7560f3f4c7e74700e49754ed101c
LunaSakura <118539965+Luna5akura@users.noreply.github.com>
2024-05-24 18:14:37 +0800

    awa

--- update_readme.py
+++ update_readme.py
@@ -1,20 +1,13 @@
-from datetime import datetime
+import datetime
 
-current_date = datetime.now().strftime('%Y-%m-%d')
+with open('README.md', 'r', encoding='utf-8') as file:
+    readme_content = file.read()
 
-base_url = "https://wakatime.com/share/@974cd1bf-f67e-44b6-b583-0472a71ce20a/22838a5c-998d-4570-afa4-0858b9d26a45.svg"
+status_message = "hapi hapi"
 
-markdown_content = f"""
-# How does pi attract others? -- Picharm!
+updated_content = readme_content.replace("{{status}}", status_message)
 
-### Status: hapi hapi
+with open('README.md', 'w', encoding='utf-8') as file:
+    file.write(updated_content)
 
-![Light theme image]({base_url}?date={current_date})
-![Dark theme image]({base_url}?date={current_date})
-"""
-
-# 将内容写入文件
-with open('README.md', 'w') as file:
-    file.write(markdown_content)
-
-print("Markdown file has been updated with the current date.")
+print("README.md has been updated.")
